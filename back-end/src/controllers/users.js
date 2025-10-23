083f32471b035a67813dd79ea170db3f056b7b71
Paulo Henrique de Andrade <paulohenrique.andradefilho@gmail.com>
2025-10-22 21:59:58 -0300

    (22/10) Proteção de rotas no back-end e no front-end

--- back-end/src/controllers/users.js
+++ back-end/src/controllers/users.js
@@ -6,7 +6,15 @@ const controller = {}     // Objeto vazio
 
 controller.create = async function (req, res) {
   try {
+    // Somente usuários administradores podem acessar este recurso
+    // HTTP 403: Forbidden
+    if (!req?.authUser?.is_admin) return res.status(403).end();
 
+    // Verifica se existe o campo "password" em "req.body".
+    // Caso positivo, geramos o hash da senha antes de enviá-la
+    // ao BD
+    // (12 na chamada a bcrypt.hash() corresponde ao número de
+    // passos de criptografia utilizados no processo)
     if (req.body.password) {
       req.body.password = await bcrypt.hash(req.body.password, 12);
     }
@@ -26,6 +34,10 @@ controller.create = async function (req, res) {
 
 controller.retrieveAll = async function (req, res) {
   try {
+    // Somente usuários administradores podem acessar este recurso
+    // HTTP 403: Forbidden
+    if (!req?.authUser?.is_admin) return res.status(403).end();
+
     const result = await prisma.user.findMany(
       // Omite o campo "password" do resultado
       // por questões de segurança
@@ -45,6 +57,13 @@ controller.retrieveAll = async function (req, res) {
 
 controller.retrieveOne = async function (req, res) {
   try {
+    // Somente usuários administradores ou o próprio usuário
+    // autenticado podem acessar este recurso
+    // HTTP 403: Forbidden
+    if (!(req?.authUser?.is_admin ||
+      Number(req?.authUser?.id) === Number(req.params.id)))
+      return res.status(403).end();
+
     const result = await prisma.user.findUnique({
       // Omite o campo "password" do resultado
       // por questões de segurança
@@ -67,7 +86,15 @@ controller.retrieveOne = async function (req, res) {
 
 controller.update = async function (req, res) {
   try {
+    // Somente usuários administradores podem acessar este recurso
+    // HTTP 403: Forbidden
+    if (!req?.authUser?.is_admin) return res.status(403).end();
 
+    // Verifica se existe o campo "password" em "req.body".
+    // Caso positivo, geramos o hash da senha antes de enviá-la
+    // ao BD
+    // (12 na chamada a bcrypt.hash() corresponde ao número de
+    // passos de criptografia utilizados no processo)
     if (req.body.password) {
       req.body.password = await bcrypt.hash(req.body.password, 12);
     }
@@ -92,6 +119,10 @@ controller.update = async function (req, res) {
 
 controller.delete = async function (req, res) {
   try {
+    // Somente usuários administradores podem acessar este recurso
+    // HTTP 403: Forbidden
+    if (!req?.authUser?.is_admin) return res.status(403).end();
+
     await prisma.user.delete({
       where: { id: Number(req.params.id) }
     })
--- front-end/src/routes/AppRoutes.jsx
+++ front-end/src/routes/AppRoutes.jsx
@@ -1,39 +1,112 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
-import Homepage from '../pages/Homepage'
+import Homepage from "../pages/Homepage";
 
-import CarForm from '../pages/car/CarForm'
-import CarList from '../pages/car/CarList'
+import CarForm from "../pages/car/CarForm";
+import CarList from "../pages/car/CarList";
 
-import CustomerForm from '../pages/customer/CustomerForm'
-import CustomerList from '../pages/customer/CustomerList'
+import CustomerForm from "../pages/customer/CustomerForm";
+import CustomerList from "../pages/customer/CustomerList";
 
-import UserList from '../pages/user/UserList'
-import UserForm from '../pages/user/UserForm'
+import UserList from "../pages/user/UserList";
+import UserForm from "../pages/user/UserForm";
 
-import Login from '../pages/Login'
+import Login from "../pages/Login";
+
+import AuthGuard from "./AuthGuard";
 
 export default function AppRoutes() {
-  return <Routes>
-    <Route path="/" element={ <Homepage /> } />
+  return (
+    <Routes>
+      <Route path="/" element={<Homepage />} />
 
-    <Route path="/login" element={ <Login /> } />
+      <Route path="/login" element={<Login />} />
 
-    <Route path="/cars" element={ <CarList /> } />
-    <Route path="/cars/new" element={ <CarForm /> } />
-    <Route path="/cars/:id" element={ <CarForm /> } />
+      <Route
+        path="/cars"
+        element={
+          <AuthGuard>
+            {" "}
+            <CarList />{" "}
+          </AuthGuard>
+        }
+      />
+      <Route
+        path="/cars/new"
+        element={
+          <AuthGuard>
+            {" "}
+            <CarForm />{" "}
+          </AuthGuard>
+        }
+      />
+      <Route
+        path="/cars/:id"
+        element={
+          <AuthGuard>
+            {" "}
+            <CarForm />{" "}
+          </AuthGuard>
+        }
+      />
 
-    <Route path="/customers" element={ 
-      <CustomerList /> 
-    } />
-    
-    <Route path="/customers/new" element={ <CustomerForm />} />
-    <Route path="/customers/:id" element={ <CustomerForm />  } />
+      <Route
+        path="/customers"
+        element={
+          <AuthGuard>
+            {" "}
+            <CustomerList />{" "}
+          </AuthGuard>
+        }
+      />
 
-    <Route path="/users" element={ <UserList /> } />
-    <Route path="/users/new" element={ <UserForm /> } />
-    <Route path="/users/:id" element={ <UserForm /> } />
-    
-  </Routes>
+      <Route
+        path="/customers/new"
+        element={
+          <AuthGuard>
+            {" "}
+            <CustomerForm />{" "}
+          </AuthGuard>
+        }
+      />
+      <Route
+        path="/customers/:id"
+        element={
+          <AuthGuard>
+            {" "}
+            <CustomerForm />{" "}
+          </AuthGuard>
+        }
+      />
+
+      <Route
+        path="/users"
+        element={
+          <AuthGuard adminOnly={true}>
+            {" "}
+            <UserList />{" "}
+          </AuthGuard>
+        }
+      />
+      <Route
+        path="/users/new"
+        element={
+          <AuthGuard adminOnly={true}>
+            {" "}
+            <UserForm />{" "}
+          </AuthGuard>
+        }
+      />
+      <Route
+        path="/users/:id"
+        element={
+          <AuthGuard adminOnly={true}>
+            {" "}
+            <UserForm />{" "}
+          </AuthGuard>
+        }
+      />
+    </Routes>
+  );
 }
