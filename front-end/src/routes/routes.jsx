30dcea9bf8294121051c107e659b963bf6b485d1
Paulo Henrique de Andrade <paulohenrique.andradefilho@gmail.com>
2025-10-29 22:08:01 -0300

    (29/10) Início da implementação de ataque de força bruta

--- front-end/src/routes/routes.jsx
+++ front-end/src/routes/routes.jsx
@@ -16,6 +16,8 @@ import CarForm from "../pages/car/CarForm";
 import UserList from "../pages/user/UserList";
 import UserForm from "../pages/user/UserForm";
 
+import BruteForce from "../pages/BruteForce";
+
 /*
  Os níveis de acesso foram definidos como segue:
  0 ~> qualquer usuário (incluindo quando não há usuário autenticado)
@@ -101,6 +103,13 @@ const routes = [
     element: <UserForm />,
     userLevel: UserLevel.ADMIN,
     omitFromMainMenu: true,
+  },
+  {
+    route: "/brute-force",
+    description: "Ataque de força bruta",
+    element: <BruteForce />,
+    userLevel: UserLevel.ADMIN,
+    divider: true,
   },
 ];
 
