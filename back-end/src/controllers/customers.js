ec0743cce4290e708f057def03a2a995345092d7
Paulo Henrique de Andrade <paulohenrique.andradefilho@gmail.com>
2025-11-05 19:54:00 -0300

    (01/11) Térino do ataque de força bruta e início da implementação de validação de dados de usuário

--- back-end/src/controllers/customers.js
+++ back-end/src/controllers/customers.js
@@ -1,24 +1,46 @@
 import prisma from '../database/client.js'
+import Customer from '../models/Customer.js'
+import { ZodError } from 'zod'
 
 const controller = {}     // Objeto vazio
 
-controller.create = async function(req, res) {
+controller.create = async function (req, res) {
   try {
 
+
+    // Sempre que houver um campo que represente uma data,
+    // precisamos garantir sua conversão para o tipo Date
+    // antes de passá-lo ao Zod para validação
+    if (req.body.birth_date) req.body.birth_date = new Date(req.body.birth_date)
+
+
+    // Invoca a validação do modelo do Zod para os dados que
+    // vieram em req.body
+    Customer.parse(req.body)
+
+
     await prisma.customer.create({ data: req.body })
+
 
     // HTTP 201: Created
     res.status(201).end()
   }
-  catch(error) {
+  catch (error) {
     console.error(error)
 
-    // HTTP 500: Internal Server Error
-    res.status(500).end()
+
+    // Se for erro de validação do Zod, retorna
+    // HTTP 422: Unprocessable Entity
+    if (error instanceof ZodError) res.status(422).send(error.issues)
+
+
+    // Senão, retorna o habitual HTTP 500: Internal Server Error
+    else res.status(500).end()
   }
 }
 
-controller.retrieveAll = async function(req, res) {
+
+controller.retrieveAll = async function (req, res) {
   try {
     const result = await prisma.customer.findMany({
       orderBy: [
@@ -32,7 +54,7 @@ controller.retrieveAll = async function(req, res) {
     // HTTP 200: OK (implícito)
     res.send(result)
   }
-  catch(error) {
+  catch (error) {
     console.error(error)
 
     // HTTP 500: Internal Server Error
@@ -40,7 +62,7 @@ controller.retrieveAll = async function(req, res) {
   }
 }
 
-controller.retrieveOne = async function(req, res) {
+controller.retrieveOne = async function (req, res) {
   try {
     const result = await prisma.customer.findUnique({
       where: { id: Number(req.params.id) },
@@ -50,11 +72,11 @@ controller.retrieveOne = async function(req, res) {
     })
 
     // Encontrou ~> retorna HTTP 200: OK (implícito)
-    if(result) res.send(result)
+    if (result) res.send(result)
     // Não encontrou ~> retorna HTTP 404: Not Found
     else res.status(404).end()
   }
-  catch(error) {
+  catch (error) {
     console.error(error)
 
     // HTTP 500: Internal Server Error
@@ -62,27 +84,50 @@ controller.retrieveOne = async function(req, res) {
   }
 }
 
-controller.update = async function(req, res) {
+controller.update = async function (req, res) {
   try {
-    const result = await prisma.customer.update({
+
+
+    // Sempre que houver um campo que represente uma data,
+    // precisamos garantir sua conversão para o tipo Date
+    // antes de passá-lo ao Zod para validação
+    if (req.body.birth_date) req.body.birth_date = new Date(req.body.birth_date)
+
+
+    // Invoca a validação do modelo do Zod para os dados que
+    // vieram em req.body
+    Customer.parse(req.body)
+
+
+    await prisma.customer.update({
       where: { id: Number(req.params.id) },
       data: req.body
     })
 
+
     // Encontrou e atualizou ~> HTTP 204: No Content
-    if(result) res.status(204).end()
-    // Não encontrou (e não atualizou) ~> HTTP 404: Not Found
-    else res.status(404).end()
+    res.status(204).end()
+
   }
-  catch(error) {
+  catch (error) {
     console.error(error)
 
-    // HTTP 500: Internal Server Error
-    res.status(500).end()
+
+    // Não encontrou e não atualizou ~> HTTP 404: Not Found
+    if (error?.code === 'P2025') res.status(404).end()
+
+
+    // Erro do Zod ~> HTTP 422: Unprocessable Entity
+    else if (error instanceof ZodError) res.status(422).send(error.issues)
+
+
+    // Outros erros ~> HTTP 500: Internal Server Error
+    else res.status(500).end()
   }
 }
 
-controller.delete = async function(req, res) {
+
+controller.delete = async function (req, res) {
   try {
     await prisma.customer.delete({
       where: { id: Number(req.params.id) }
@@ -91,8 +136,8 @@ controller.delete = async function(req, res) {
     // Encontrou e excluiu ~> HTTP 204: No Content
     res.status(204).end()
   }
-  catch(error) {
-    if(error?.code === 'P2025') {
+  catch (error) {
+    if (error?.code === 'P2025') {
       // Não encontrou e não excluiu ~> HTTP 404: Not Found
       res.status(404).end()
     }
